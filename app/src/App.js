5792bf13e7130cb62a9f6a8dec063ee14a6b6370
aabia <aabia@umich.edu>
2024-06-17 21:28:07 -0700

    added toggling sidebar

--- app/src/App.js
+++ app/src/App.js
@@ -1,21 +1,37 @@
+// App.js
+import React, { useState } from 'react';
 import logo from './aabia.jpg';
 import './App.css';
+import Sidebar from './Sidebar';
 
 function App() {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p style={{fontFamily: 'Fira Sans'}}>
-         Hello my name is Aabia, I'm a BME and CSE student at UofM!
-        </p>
-        <p style={{fontFamily: 'Fira Sans'}}>
-        Website Under Construction
-        </p>
-        <a href="https://www.linkedin.com/in/aabiahasan/">
-        <i class="fab fa-linkedin"></i>
-        </a>
-      </header>
+      <Sidebar isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
+      <div className={`container ${isSidebarOpen ? 'sidebar-open' : ''}`}>
+        <header className="App-header">
+          <div className="content">
+            <img src={logo} className="App-logo" alt="logo" />
+            <div>
+              <p style={{ fontFamily: 'Fira Sans' }}>
+                Hello my name is Aabia, I'm a BME and CSE student at UofM!
+              </p>
+              <p style={{ fontFamily: 'Fira Sans' }}>
+                Website Under Construction
+              </p>
+              <a href="https://www.linkedin.com/in/aabiahasan/">
+                <i className="fab fa-linkedin"></i>
+              </a>
+            </div>
+          </div>
+        </header>
+      </div>
     </div>
   );
 }
---
+++ app/src/Sidebar.js
@@ -0,0 +1,25 @@
+// Sidebar.js
+import React from 'react';
+import './Sidebar.css';
+
+const Sidebar = ({ isOpen, toggleSidebar }) => {
+  return (
+    <div className={`sidebar-container ${isOpen ? 'open' : ''}`}>
+      <div className={`sidebar ${isOpen ? 'open' : ''}`}>
+        <button className="close-btn" onClick={toggleSidebar}>×</button>
+        <ul>
+          <li><a href="#home">Home</a></li>
+          <li><a href="#services">Services</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+      </div>
+      {!isOpen && (
+        <button className="open-btn" onClick={toggleSidebar}>
+          ☰
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default Sidebar;
